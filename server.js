76ca9f8e8c774f2c171c5d48e9926db035f87927
hiroyukimikami <h.mikami@gmail.com>
2013-02-22 17:27:25 +0900

    Update server.js

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello world');
+  response.end('Hello ninjaja');
 }).listen(8080);
