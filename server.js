248d4881fa72f68ae80f59e96644d6d30760eca5
Hiroyuki Mikami <h.mikami@gmail.com>
2013-02-22 18:04:37 +0900

    test post

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
-var http = require('http');
+var http = require("http");
+var querystring = require("querystring");
 
 http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello Ninja');
+	var postdata = "";
+	request.setEncoding("utf-8");
+	request.on("data", function(chunk){
+		postdata += chunk;
+	});
+	request.on("end", function(){
+		response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
+		var param = querystring.parse(postdata)["name"];
+		response.end("Hello "+param);
+	});
+	
 }).listen(8080);
