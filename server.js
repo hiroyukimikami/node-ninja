d92cd242b28ea1f5dd99d0249ac84306bfe442ae
Hiroyuki Mikami <h.mikami@gmail.com>
2013-02-22 17:40:45 +0900

    test

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http');
 
 http.createServer(function (request, response) {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello ninjaja');
+  response.end('Hello Ninja');
 }).listen(8080);
